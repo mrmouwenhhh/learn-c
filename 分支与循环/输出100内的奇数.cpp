6c948eaa00deea47d6150ad38bbd025d244300f6
Mou_Wen <2065674161@qq.com>
2023-01-10 19:28:05 +0800

    测试5.0

--- 分支与循环/输出100内的奇数.cpp
+++ 分支与循环/输出100内的奇数.cpp
@@ -1,15 +1,15 @@
-#define  _CRT_SECURE_NO_WARNINGS 1
-
-#include<stdio.h>
-
-int main()
-{
-	int i = 0;
-	while (i <= 100)
-	{
-		if (i % 2 == 1)
-			printf("%d\n", i);
-		i++;
-	}
-	return 0;
-}
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}

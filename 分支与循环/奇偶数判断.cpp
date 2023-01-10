39850700a7392314b81e0a9773923741923331e8
Mou_Wen <2065674161@qq.com>
2023-01-10 18:55:58 +0800

    测试3.0

---
+++ 分支与循环/奇偶数判断.cpp
@@ -0,0 +1,17 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int be = a % 2;
+//
+//	if (be == 1)
+//		printf("是奇数");
+//	else
+//		printf("是偶数");
+//
+//	return 0;
+//}
